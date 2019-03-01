55cb926b585a01e8e2fd2b21dc49b7a055710b7a
walgarch <32451330+walgarch@users.noreply.github.com>
2019-03-01 13:24:25 +1100

    Update 012_conditional_operators.ts
    Code change to allow more feedback when running the code

--- 012_conditional_operators.ts
+++ 012_conditional_operators.ts
@@ -1,29 +1,31 @@
 let x : number = 100;
 
-// if(x == 200) {
-// 	console.log('Condition passed');
-// }
+console.log(`The number set is ${x}`);
 
-// if(x === 100) {
-// 	console.log('Condition passed');
-// }
+if(x == 200) {
+	console.log(`Condition passed : x == 200`);
+}
 
-// if(x != 100) {
-// 	console.log('Condition passed');
-// }
+if(x === 100) {
+	console.log(`Condition passed : x === 100`);
+}
 
-// if(x > 100) {
-// 	console.log('Condition passed');
-// }
+if(x != 100) {
+	console.log(`Condition passed : x != 100`);
+}
 
-// if(x >= 100) {
-// 	console.log('Condition passed');
-// }
+if(x > 100) {
+	console.log(`Condition passed:- x > 100`);
+}
 
-// if(x < 100) {
-// 	console.log('Condition passed');
-// }
+if(x >= 100) {
+	console.log(`Condition passed : x >= 100`);
+}
 
-// if(x <= 100) {
-// 	console.log('Condition passed');
-// }
+if(x < 100) {
+	console.log(`Condition passed:- x < 100`);
+}
+
+if(x <= 100) {
+	console.log(`Condition passed : x <= 100`);
+}
